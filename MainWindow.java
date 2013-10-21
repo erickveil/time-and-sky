e68eb77d003eefdce6ec1617db44ecf64f6c71fd
Erick Veil <registry@erickveil.com>
2013-10-21 10:30:27 -0700

    Set properties on main window
    I now have control over size and color :)
    
    Next, I draw stars!

--- MainWindow.java
+++ MainWindow.java
@@ -6,21 +6,15 @@ import javax.swing.*;
  */
 public class MainWindow {
     /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
+     *
      */
     public void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("FrameDemo");
+
+        JFrame frame = new JFrame("A Story of Time and The Sky");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.black);
+        frame.setSize(new Dimension(800,600));
 
-        JLabel emptyLabel = new JLabel("");
-        emptyLabel.setPreferredSize(new Dimension(175, 100));
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-
-        //Display the window.
-        frame.pack();
         frame.setVisible(true);
     }
 
